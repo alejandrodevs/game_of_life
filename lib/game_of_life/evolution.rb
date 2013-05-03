45af79c28745baf48f3ef29513204c8115b80abc
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-05-03 10:22:15 -0500

    Separating adjustment method in other class

--- lib/game_of_life/evolution.rb
+++ lib/game_of_life/evolution.rb
@@ -24,19 +24,7 @@ module GameOfLife
     end
 
     def adjustment
-      generation.each do |cell|
-        adjust_cell_neighborhood(cell)
-      end
-    end
-
-    def adjust_cell_neighborhood cell
-      cell.neighbors_positions.each do |pos|
-        adjust_neighbor(cell, pos) if cell.send(pos).nil?
-      end
-    end
-
-    def adjust_neighbor cell, pos
-
+      Adjustment.new(generation).adjust
     end
 
   end
